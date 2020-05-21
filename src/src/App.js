c009923e2a9b28732106a769684d0c8d0a1dd708
Elnur <elnur@MacBook-Pro-Elnur.local>
2020-05-22 01:39:27 +0400

    RT-3 create registration modal page

--- src/src/App.js
+++ src/src/App.js
@@ -1,24 +1,49 @@
 import React from 'react';
-import logo from './logo.svg';
+import Input from './construct/input/input'
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className = 'container'>
+      <h1 className = 'container__title'>Contact blank</h1>
+      <form>
+        <p className = 'container__text'>Name :</p>
+        <Input
+        type = {'text'}
+        className = {'container__params'}
+        placeholder = {'Insert your name'}
+        />
+        <p className = 'container__text'>e-mail :</p>
+        <Input
+        type = {'email'}
+        className = {'container__params'}
+        placeholder = {'Insert your mail'}
+        />
+        <p className = 'container__text'>Age :</p>
+        <Input
+        type  = {'number'}
+        className = {'container__params'}
+        placeholder = {'Set your age'}
+        />
+        <p className = 'container__text'>Telephone number :</p>
+        <Input
+        type = {'tel'}
+        className = {'container__params'}
+        placeholder = {'Input your number'}
+        />
+        <div className = 'container__buttons'>
+          <Input
+          type = {'submit'}
+          className = {'container__buttons-submit'}
+          value = {'Send'}
+          />
+          <Input
+          type = {'reset'}
+          className = {'container__buttons-reset'}
+          value = {'reset'}
+          />
+        </div>
+      </form>
     </div>
   );
 }
